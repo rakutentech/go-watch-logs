615ef1638792343b62dc5ee4a6d85852417d86fe
Pulkit Kathuria <kevincobain2000@gmail.com>
2025-01-21 05:12:45 +0900

    feat: messaging and fmts

--- pkg/watcher.go
+++ pkg/watcher.go
@@ -120,15 +120,14 @@ func (w *Watcher) Scan() (*ScanResult, error) {
 	isFirstScan := w.getScanCount() == 0
 
 	for scanner.Scan() {
+		line := scanner.Bytes()
+		bytesRead += int64(len(line)) + 1 // Adding 1 for the newline character
 		currentLineNum++
 		linesRead = currentLineNum - w.lastLineNum
 		// Convert to positive number
 		if linesRead < 0 {
 			linesRead = -linesRead
 		}
-
-		line := scanner.Bytes()
-		bytesRead += int64(len(line)) + 1 // Adding 1 for the newline character
 
 		if isFirstScan {
 			continue
