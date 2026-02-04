d76cbb3758515db315b516e4e04e2daa3a6da505
Pulkit Kathuria <kevincobain2000@gmail.com>
2026-02-04 13:48:31 +0900

    ci: lint

--- pkg/pagerduty_test.go
+++ pkg/pagerduty_test.go
@@ -13,7 +13,7 @@ type mockHTTPClient struct {
 	err      error
 }
 
-func (m *mockHTTPClient) Do(req *http.Request) (*http.Response, error) {
+func (m *mockHTTPClient) Do(_ *http.Request) (*http.Response, error) {
 	if m.err != nil {
 		return nil, m.err
 	}
@@ -26,7 +26,7 @@ type mockTransport struct {
 	err      error
 }
 
-func (m *mockTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+func (m *mockTransport) RoundTrip(_ *http.Request) (*http.Response, error) {
 	if m.err != nil {
 		return nil, m.err
 	}
